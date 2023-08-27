4aecdf89d14315859a6063a7a84a4d031485d5ca
Thúlio Gabriel <thulioggomes@gmail.com>
2023-08-26 21:41:11 -0300

    Pyhton automatizado com pyautogui

---
+++ app.py
@@ -0,0 +1,19 @@
+import pyautogui
+from time import sleep
+
+#navgador 107,15
+pyautogui.click(107,15,duration=1)
+#contato 373,318
+pyautogui.click(373,318,duration=1)
+# barra de msg 795,990
+pyautogui.click(795,990,duration=1)
+pyautogui.write('Esta tudo OK')
+
+# apertar no enviar 
+pyautogui.click(1725,981,duration=1)
+
+
+
+        
+
+
